626679ab88793dd8d17d20e179815d2650defc6e
Amritgrg1 <gamrit450@gmail.com>
2023-02-14 02:08:45 +0545

    java assignment

---
+++ Amrit.java
@@ -0,0 +1,17 @@
+public class Amrit{
+    public static void main(String[] args){
+        System.out.println("Name of the organization: Aarughat hardware shop");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Address:");
+        System.out.println("----state: Gorkha");
+        System.out.println("----zip code: 34000");
+        System.out.println("----country: Nepal");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Contact Information:");
+        System.out.println("----Mobile Number: 9861112300");
+        System.out.println("----Email: gorkha@gmail.com");
+
+    }
+}
